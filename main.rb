fcf470cde84bc226cf3a84a149cddb65429eb2c5
obliviusm <obliviusm@gmail.com>
2016-08-31 15:09:29 +0300

    move env vars

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@
 require 'gracenote'
 
 spec = {
-  clientID: "883223413",
-  clientTag: "810484BCF1F454CF0A456E133A855E02",
-  userID: "280164029668204488-CAC04155321454D0490E369346F50E8D"
+  clientID: ENV["CLIENT_ID"],
+  clientTag: ENV["CLIENT_TAG"],
+  userID: ENV["USER_ID"]
 }
 obj = Gracenote.new(spec)
 # obj.registerUser # to get userID
@@ -15,6 +15,7 @@ p "---------------------------------"
 p "---------------------------------"
 p "---------------------------------"
 
-# print obj.findTrack("Kings Of Convenience", "", "Home", '0').to_yaml
+print obj.findTrack("Kings Of Convenience", "", "Homesick", '0').to_yaml
 
-print obj.findTrack("Whitey", "", "", '0').to_yaml
+# print obj.findTrack("Whitey", "", "", '0').to_yaml
+# p obj.findTrack("Kings Of Convenience", "", "Homesick", '0').inspect
