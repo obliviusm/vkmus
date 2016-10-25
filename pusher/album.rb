b7c14a21774c343eff9458a1ad9c6d29a80a2c9d
obliviusm <obliviusm@gmail.com>
2016-10-25 13:56:34 +0300

    don't create album if present

--- pusher/album.rb
+++ pusher/album.rb
@@ -1,5 +1,15 @@
 require_relative '../require_all_helper'
 
+def album_create_or_get app, album_title
+  album_hashes = app.audio.getAlbums()["items"]
+  album = album_hashes.keep_if { |a| a["title"] == album_title }[0]
+  if album
+    album["id"]
+  else
+    app.audio.addAlbum(title: album_title)["album_id"]
+  end
+end
+
 SOURCE_NAME = "output/filter/genre/60's_rock"
 ALBUM_TITLE = "60s Rock"
 
@@ -9,5 +19,5 @@ song_ids = songs.map{ |s| s["id"] }.join(",")
 # p song_ids
 
 app = VK::Application.new(version: '5.53', access_token: ENV['VK_ACCESS_TOKEN'])
-new_album_id = app.audio.addAlbum(title: ALBUM_TITLE)["album_id"]
-app.audio.moveToAlbum(album_id: new_album_id, audio_ids: song_ids)
+album_id = album_create_or_get app, ALBUM_TITLE
+app.audio.moveToAlbum(album_id: album_id, audio_ids: song_ids)
